efb602a2ce0e10b32556e1835bc6106a1086669f
thornbird <thornbird.blue@gmail.com>
2022-07-12 16:55:27 +0800

    ci compile ok code.

---
+++ app/src/main/java/com/example/tb/MyActivity.java
@@ -0,0 +1,12 @@
+package com.example.tb;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MyActivity extends Activity {
+	protected void onCreate(Bundle saveInstanceState)
+	{
+		super.onCreate(saveInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
